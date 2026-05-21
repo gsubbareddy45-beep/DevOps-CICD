80cd808947d6c693440e3bbe82d1231082659290
gsubbareddy45-beep <gsubbareddy45@gmail.com>
2026-05-21 16:21:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Flask CI/CD Deployment Successful "
+    return "Flask CI/CD Deployment Succes"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
